55627bc2cf38fa29407906486cb78acaa50e1f88
Heri Susanto <herisusanto.app@gmail.com>
2024-04-05 13:17:28 +0700

    Final push

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -56,6 +56,7 @@ export default function Home() {
         <ActivityIndicator style={styles.spinner} />
       ) : (
         <FlatList
+          testID=""
           data={contacts}
           renderItem={({ item }) => (
             <CardContact
